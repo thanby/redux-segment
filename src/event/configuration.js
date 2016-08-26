5637aef3554d9b242a6eb4332c4553ebb79b74e8
bertrandk <bertrandk@outlook.com>
2016-08-25 20:22:57 -0400

    Capture react-router-redux's change event (#62)
    Closes #59.

--- src/event/configuration.js
+++ src/event/configuration.js
@@ -5,6 +5,7 @@ const defaultMapper = {
   mapper: {
     '@@router/INIT_PATH': page,
     '@@router/UPDATE_PATH': page,
+    '@@router/LOCATION_CHANGE': page,
     '@@reduxReactRouter/initRoutes': page,
     '@@reduxReactRouter/routerDidChange': page,
     '@@reduxReactRouter/replaceRoutes': page,
